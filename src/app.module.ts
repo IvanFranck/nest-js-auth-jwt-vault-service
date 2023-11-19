8ba3c3f4e06aedf2c66a57d52c5a19ccfa8d0b50
ivan nzima <nzimaivan007@gmail.com>
2023-11-19 17:26:58 +0100

    seting up confgimodule and env var validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { VaultModule } from './vault/vault.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [AuthModule, UsersModule, VaultModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    VaultModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        VAULT_TOKEN: Joi.string().required(),
+        VAULT_ADDR: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
